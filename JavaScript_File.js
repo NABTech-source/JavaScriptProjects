bf9edbe358452c797e093fa5b7d488b15300589e
NABTech-source <nabtech87@gmail.com>
2025-11-10 01:53:33 -0500

    Add files via upload
    Java Script file

---
+++ JavaScript_File.js
@@ -0,0 +1,14 @@
+alert('Hello World');
+
+
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
+var B="10"+"Nadeem"
+document.write(B)
+
+var A="Hi Nadeem";
+
+document.write(A);
